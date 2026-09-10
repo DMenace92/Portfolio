bb05212b9fa7e236df18709787b5b98972280e5b
DMenace92 <dennisenwiya@gmail.com>
2026-09-09 18:26:34 -0700

    added updates

--- Portfolio/src/utils.js
+++ Portfolio/src/utils.js
@@ -1,5 +1,4 @@
-
-const api_connect = 'http://localhost:9000'
+const api_connect = 'https://dennisenwiya.dev/'
 // const api_connect = 'https://myportfolio-api-44ada9ce9fb4.herokuapp.com/'
 
-export default api_connect;
+export default api_connect
